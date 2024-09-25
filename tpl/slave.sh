57edfbe74ac93de6db5d3d1c606fff70b85a1f1a
patod01 <patocv2@hotmail.com>
2024-09-25 11:45:22 -0300

    Verbosing the log

--- tpl/slave.sh
+++ tpl/slave.sh
@@ -2,6 +2,6 @@ if [[ $1 = "job" ]]; then
      cd %s
      source .venv/bin/activate
      printf "\n$(date)\n" >> built/longi.txt
-     pytest -k $2 >> built/longi.txt
+     pytest -k $2 -v >> built/longi.txt
      deactivate
 fi
