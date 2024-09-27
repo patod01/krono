5538ba375d3828cab5331a75b9e0f265b9011dc0
patod01 <patocv2@hotmail.com>
2024-09-27 15:44:02 -0300

    Log output filtered

--- tpl/slave.sh
+++ tpl/slave.sh
@@ -3,5 +3,9 @@ if [[ $1 = "job" ]]; then
      source .venv/bin/activate
      printf "\n$(date)\n" >> built/longi.txt
      pytest -k $2 -v >> built/longi.txt
+     pytest -k delay -v \
+     | grep -v platform \
+     | grep -v cachedir \
+     | grep -v rootdir >> built/longi.txt
      deactivate
 fi
