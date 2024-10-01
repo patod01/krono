5378ca208786c32a1b66e2baafd8c641f5ef34bb
patod01 <patocv2@hotmail.com>
2024-10-01 20:05:42 -0300

    Yet better logs...

--- tpl/slave.sh
+++ tpl/slave.sh
@@ -5,6 +5,7 @@ if [[ $1 = "job" ]]; then
      pytest -k $2 -v \
      | grep -v platform \
      | grep -v cachedir \
-     | grep -v rootdir >> built/longi.txt
+     | grep -v rootdir \
+     | grep -v '^$' >> built/longi.txt
      deactivate
 fi
